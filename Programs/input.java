9a3a4458e5191240a040945d02cb6f5f690d9c39
harsh6125 <hv7484@gmail.com>
2026-07-15 12:06:25 +0530

    added a new program

--- Programs/input.java
+++ Programs/input.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class input {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter your age, height, grade, and name: ");
+            int age = sc.nextInt();
+            float height = sc.nextFloat();
+            char grade = sc.next().charAt(0);
+            String name = sc.next();
 
-        System.out.print("Enter your age, height, grade, and name: ");
-        int age = sc.nextInt();
-        float height = sc.nextFloat();
-        char grade = sc.next().charAt(0);
-        String name = sc.next();
-
-        System.out.println("Your age: " + age);
-        System.out.println("Your height: " + height);
-        System.out.println("Your grade: " + grade);
-        System.out.println("Your name: " + name);
-        sc.close();
+            System.out.println("Your age: " + age);
+            System.out.println("Your height: " + height);
+            System.out.println("Your grade: " + grade);
+            System.out.println("Your name: " + name);
+        }
     }
 }
